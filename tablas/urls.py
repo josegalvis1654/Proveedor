33c686a3c4ab60aaa738de35229ca54fc24a332f
josegalvis1654 <jgalvis19@udi.edu.co>
2024-11-05 08:22:16 -0500

    Segundo Avance CRUD

--- tablas/urls.py
+++ tablas/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import Prov
 
 urlpatterns = [
-    path('proveedores/', views.proveedores_list, name='proveedores_list'),
-    path('proveedores/<int:pk>/', views.proveedor_detail, name='proveedor_detail'),
+    path('proveedores/', Prov.crear, name='crear_proveedor'),
+    path('proveedores/<int:id>/', Prov.eliminar, name='eliminar_proveedor'),
 ]
--- tablas/views.py
+++ tablas/views.py
@@ -1,41 +1,24 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import Proveedores
 from .serializers import ProveedoresSerializer
 
-@api_view(['GET', 'POST'])
-def proveedores_list(request):
-    if request.method == 'GET':
-        proveedores = Proveedores.objects.all()
-        serializer = ProveedoresSerializer(proveedores, many=True)
-        return Response(serializer.data)
+class ProveedorViewSet(viewsets.ModelViewSet):
+    queryset = Proveedores.objects.all()
+    serializer_class = ProveedoresSerializer
 
-    elif request.method == 'POST':
-        serializer = ProveedoresSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def proveedor_detail(request, pk):
-    try:
-        proveedor = Proveedores.objects.get(pk=pk)
-    except Proveedores.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = ProveedoresSerializer(proveedor)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = ProveedoresSerializer(proveedor, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        proveedor.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class Prov:
+    def crear(self, request):
+        if request.method == 'POST':
+            proveedor = Proveedores(
+            nit=request.POST['nit'],
+            razon_social=request.POST['razon_social'],
+            representante_legal=request.POST['representante_legal'],
+            direccion=request.POST['direcion'], 
+            vehiculo_asociado=request.POST['vehiculo_asociado']
+            )
+            proveedor.save()
+            return Response({'error': 'Faltan datos necesarios'}, status=400)
+    def eliminar(self, request, id):
+        proveedor = Proveedores.objects.get(id=id)
+        proveedor.delete
