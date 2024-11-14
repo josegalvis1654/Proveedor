c7f9e5064749e53da969316d0a106d10ef90270f
SuarezJr <esuarez14@udi.edu.co>
2024-11-14 09:05:56 -0500

    Corrección models.py
    Se corrigió el archivo models.py para aumentar el tamaño de los campos en la BD

--- tablas/models.py
+++ tablas/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-
 class Proveedores(models.Model):
     nit = models.CharField(max_length=10)
-    razon_social = models.CharField(max_length=20)
-    representante_legal= models.CharField(max_length=20)
-    direcion = models.CharField(max_length=10)
-    telefono = models.IntegerField()
-    vehiculo_asociado = models.CharField(max_length=20)
+    razon_social = models.CharField(max_length=200)
+    representante_legal= models.CharField(max_length=200)
+    direcion = models.CharField(max_length=100)
+    telefono = models.PositiveBigIntegerField()
+    vehiculo_asociado = models.CharField(max_length=200)
